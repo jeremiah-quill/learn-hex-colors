c3323971c309cbac5375a859050b442a34b2e95c
Jeremiah Quill <jcq5010@gmail.com>
2022-11-18 20:28:33 -0500

    changed variable names to better describe what they are

--- components/Game.jsx
+++ components/Game.jsx
@@ -3,13 +3,22 @@ import Button from "../components/Button";
 import Timer from "../components/Timer";
 import GameOver from "../components/GameOver";
 import { useEffect, useState } from "react";
-import uuid from "react-uuid";
+import { shuffle } from "../utils";
 
 export function Game() {
+  // const [rounds, setRounds] = useState(12);
+  // const [round, setRound] = useState(1);
+  // const [tries, setTries] = useState(2);
+  // const [colors, setColors] = useState([]);
+  // const [correctColor, setCorrectColor] = useState("");
+  // const [correctColorIndex, setCorrectColorIndex] = useState(0);
+  // const [score, setScore] = useState(0);
+  // const [gameOver, setGameOver] = useState(false);
+
   const [isGameOver, setIsGameOver] = useState(false);
-  const [hexSelections, setHexSelections] = useState([]);
+  const [currentOptions, setCurrentOptions] = useState([]);
   const [timeLeft, setTimeLeft] = useState(60);
-  const [results, setResults] = useState([]);
+  const [rounds, setRounds] = useState([]);
   const [guessCount, setGuessCount] = useState(0);
   const [penalized, setPenalized] = useState(false);
 
@@ -19,8 +28,8 @@ export function Game() {
 
   function resetGame() {
     setIsGameOver(false);
-    setHexSelections([]);
-    setResults([]);
+    setCurrentOptions([]);
+    setRounds([]);
     setTimeLeft(60);
     nextColor();
   }
@@ -34,36 +43,17 @@ export function Game() {
     return color;
   }
 
-  // fischer yates shuffle
-  function shuffle(array) {
-    let currentIndex = array.length,
-      randomIndex;
-
-    // While there remain elements to shuffle.
-    while (currentIndex != 0) {
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-    }
-
-    return array;
-  }
-
   function nextColor() {
-    const questionId = uuid();
     const randomColor = getRandomColor();
-    const newResult = { color: randomColor, guesses: [], id: questionId, isCorrect: false };
+    const newRound = { color: randomColor, guesses: [], isCorrect: false };
 
-    setResults((currResults) => [...currResults, newResult]);
+    setRounds((currRounds) => [...currRounds, newRound]);
     const selections = [
-      { color: getRandomColor(), isShowing: true, isCorrect: false, questionId },
-      { color: getRandomColor(), isShowing: true, isCorrect: false, questionId },
-      { color: randomColor, isShowing: true, isCorrect: true, questionId },
+      { color: getRandomColor(), isGuessed: false, isCorrect: false },
+      { color: getRandomColor(), isGuessed: false, isCorrect: false },
+      { color: randomColor, isGuessed: false, isCorrect: true },
     ];
-    setHexSelections(shuffle(selections));
+    setCurrentOptions(shuffle(selections));
   }
 
   function penalizeTime(seconds) {
@@ -75,74 +65,70 @@ export function Game() {
   }
 
   function handleSelection(color) {
-    setResults((currResults) =>
-      currResults.map((result) =>
-        result.id === color.questionId
+    setRounds((currRounds) =>
+      currRounds.map((round, idx) =>
+        idx === currRounds.length - 1
           ? {
-              ...result,
-              isCorrect: color.isCorrect ? true : false,
-              guesses: [
-                ...result.guesses,
-                { color: color.color, isCorrect: color.isCorrect ? true : false },
-              ],
+              ...round,
+              isCorrect: color.isCorrect,
+              guesses: [...round.guesses, { color: color.color, isCorrect: color.isCorrect }],
             }
-          : result
+          : round
       )
     );
+
     if (color.isCorrect) {
       nextColor();
       setGuessCount(0);
-    } else {
-      penalizeTime(10);
-      if (guessCount === 1) {
-        nextColor();
-        setGuessCount(0);
-      } else {
-        setGuessCount((prevCount) => prevCount + 1);
-        setHexSelections((hexSelections) => {
-          return hexSelections.map((selection) => {
-            if (selection.color === color.color) {
-              return { ...selection, isShowing: false };
-            }
-            return selection;
-          });
-        });
-      }
+      return;
     }
+    if (!color.isCorrect && guessCount === 0) {
+      penalizeTime(10);
+      setGuessCount((prevCount) => prevCount + 1);
+      setCurrentOptions((prevOptions) =>
+        prevOptions.map((option) =>
+          option.color === color.color ? { ...option, isGuessed: true } : option
+        )
+      );
+      return;
+    }
+    penalizeTime(10);
+    nextColor();
+    setGuessCount(0);
   }
 
-  if (isGameOver) return <GameOver results={results} onReset={resetGame} />;
+  if (isGameOver) return <GameOver results={rounds} onReset={resetGame} />;
 
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="flex justify-between w-full items-end">
-        <div className="grid grid-cols-12 h-[56px] w-full absolute top-0 bg-zinc-100">
-          <ColorTracker results={results} />
-        </div>
-        <Timer
-          time={timeLeft}
-          onTick={() => setTimeLeft((prevTimeLeft) => prevTimeLeft - 1)}
-          onTimerEnd={() => setIsGameOver(true)}
-          classes={
-            "text-4xl font-bold absolute right-0 h-[56px] flex items-center justify-center px-4"
-          }
-          penalized={penalized}
-        />
+      <div className="grid grid-cols-12 h-[56px] w-full absolute top-0 bg-zinc-100">
+        <ColorTracker results={rounds} />
       </div>
+      <Timer
+        time={timeLeft}
+        onTick={() => setTimeLeft((prevTimeLeft) => prevTimeLeft - 1)}
+        onTimerEnd={() => setIsGameOver(true)}
+        classes={
+          "text-4xl font-bold absolute right-0 h-[56px] flex items-center justify-center px-4"
+        }
+        penalized={penalized}
+      />
       <div
-        style={{ backgroundColor: results[results.length - 1]?.color }}
+        style={{ backgroundColor: rounds[rounds.length - 1]?.color }}
         className={`w-full h-full flex-1`}
       />
 
       <div className="grid sm:grid-cols-3 w-full mx-auto auto-cols-auto h-[200px]">
-        {hexSelections.map((hex, idx) => (
+        {currentOptions.map((hex, idx) => (
           <Button
             key={idx}
-            classes={`font-bold cursor-pointer text-5xl ${
-              !hex.isShowing ? "bg-red-300 opacity-50" : "hover:bg-slate-200"
+            classes={`font-bold text-5xl ${
+              hex.isGuessed
+                ? "bg-red-300 opacity-50 cursor-disabled"
+                : "hover:bg-slate-200 cursor-pointer"
             }`}
-            onClick={() => (hex.isShowing ? handleSelection(hex) : null)}
-            disabled={hex.isShowing ? false : true}>
+            onClick={() => (hex.isGuessed ? null : handleSelection(hex))}
+            disabled={hex.isGuessed}>
             {hex.color}
           </Button>
         ))}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,31 @@
-import { LearnColors } from "../components/LearnColors";
+import { useState } from "react";
+import Link from "next/link";
+
+import { HexColorPicker } from "react-colorful";
+
+import { FaRegHandPointDown } from "react-icons/fa";
+import { FaPlay } from "react-icons/fa";
 
 export default function Home() {
-  return <LearnColors />;
+  const [color, setColor] = useState("#1880ff"); // control color picker
+  return (
+    <div style={{ backgroundColor: color }} className="min-h-screen">
+      <div className="max-w-5xl mx-auto pt-32 ">
+        <h1 className="text-4xl md:text-6xl font-bold font-inter text-zinc-900 bg-zinc-100 p-4">
+          Tired of looking up colors? Take a few minutes and learn your hex codes.{" "}
+        </h1>
+        <FaRegHandPointDown className="text-6xl text-black w-full text-center bg-zinc-100" />
+        <HexColorPicker color={color} onChange={setColor} style={{ width: "100%" }} />
+        <div className="grid sm:grid-cols-2 text-zinc-900 bg-zinc-100">
+          <h2 className="text-5xl font-bold p-4">Hex Code: {color}</h2>
+          <Link href="/play">
+            <div className="text-3xl font-bold transition-all flex h-full items-center justify-center gap-4 hover:bg-zinc-300">
+              Play
+              <FaPlay className="text-zinc-900 text-2xl" />
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
